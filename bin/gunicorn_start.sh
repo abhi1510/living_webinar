22fcc25b80d703b266b30ad4bcb5f5862d53edfa
Abhinav Kumar <abhik.chy@gmail.com>
2019-11-04 18:43:04 +0530

    added deployemnt

--- bin/gunicorn_start.sh
+++ bin/gunicorn_start.sh
@@ -1,6 +1,9 @@
-NAME="livingwebinar"                                            # Name of the application
+#!/bin/bash
+  
+NAME="living_webinar"                                           # Name of the application
 DJANGODIR=/home/ubuntu/living_webinar/web                       # Django project directory
-SOCKFILE=/home/ubuntu/living_webinar/gunicorn.sock              # we will communicte using this unix socket
+SOCKFILE=/home/ubuntu/living_webinar/run/app.sock               # we will communicte using this unix socket
+USER=ubuntu                                                     # the user to run as
 NUM_WORKERS=3                                                   # how many worker processes should Gunicorn spawn
 DJANGO_SETTINGS_MODULE=web.settings.production                  # which settings file should Django use
 DJANGO_WSGI_MODULE=web.wsgi                                     # WSGI module name
@@ -11,7 +14,6 @@ echo "Starting $NAME as `whoami`"
 cd $DJANGODIR
 source ../../lw_venv/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
-export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 
 # Create the run directory if it doesn't exist
 RUNDIR=$(dirname $SOCKFILE)
@@ -19,8 +21,9 @@ test -d $RUNDIR || mkdir -p $RUNDIR
 
 # Start your Django Unicorn
 exec /home/ubuntu/lw_venv/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
- --name $NAME \
- --workers $NUM_WORKERS \
- --bind=$SOCKFILE \
- --log-level=debug \
- --log-file=-
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user=$USER \
+  --bind=unix:$SOCKFILE \
+  --log-level=debug \
+  --log-file=-
