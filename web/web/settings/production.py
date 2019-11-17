81e07a2b63fbcf9c95aad3b96825486a598a5946
Abhinav Kumar <abhik.chy@gmail.com>
2019-11-17 19:44:22 +0530

    added production db and changed host

--- web/web/settings/production.py
+++ web/web/settings/production.py
@@ -1,4 +1,14 @@
 from .base import *
 
-# TODO: change this to prod URL
-HOST = 'http://localhost:8001'
+HOST = 'http://ec2-18-190-105-126.us-east-2.compute.amazonaws.com'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'livingwebinar_db',
+        'HOST': 'database-livingwebinar.cg5pev8vjomf.us-east-2.rds.amazonaws.com',
+        'PORT': 3306,
+        'USER': 'lw_admin',
+        'PASSWORD': 'secret2success'
+    }
+}
